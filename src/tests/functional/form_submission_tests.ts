07feddf0bd7ac0afb9833306bd8bf24ccfac6889
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-14 12:29:04 -0500

    Enable opting-out of form submissions
    Similar to [Disabling Turbo Drive on Specific Links][turbo-drive-false],
    guard intercepting form subissions with a similar check for
    `[data-turbo-drive="false"]` on the submitter, `<form>` element, or an
    ancestor.
    
    Similarly, *do not intercept* when the submitter or form element
    specifies a method that isn't an HTTP verb (via
    [formmethod="dialog"][formmethod] attribute or [method="dialog"][method]
    attribute, respectively).
    
    In practice, `<form>` elements will only ever declare `method="get"` or
    `method="post"`, but given the addition of `method="dialog"` and the
    fact that the `fetchMethodFromString` and the `FetchMethod` enum type
    exist, we might as well be careful and future-proof.
    
    [turbo-drive-false]: https://turbo.hotwire.dev/handbook/drive#disabling-turbo-drive-on-specific-links
    [formmethod]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#attr-formmethod
    [method]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form#attr-method

--- src/tests/functional/form_submission_tests.ts
+++ src/tests/functional/form_submission_tests.ts
@@ -44,6 +44,68 @@ export class FormSubmissionTests extends TurboDriveTestCase {
     this.assert.equal(await message.getVisibleText(), "Hello!")
     this.assert.equal(await this.pathname, "/src/tests/fixtures/form.html")
   }
+
+  async "test form submission with Turbo disabled on the form"() {
+    this.listenForFormSubmissions()
+    await this.clickSelector('#disabled form[data-turbo="false"] input[type=submit]')
+    await this.nextBody
+    await this.querySelector("#element-id")
+
+    this.assert.notOk(await this.turboFormSubmitted)
+  }
+
+  async "test form submission with Turbo disabled on the submitter"() {
+    this.listenForFormSubmissions()
+    await this.clickSelector('#disabled form:not([data-turbo]) input[data-turbo="false"]')
+    await this.nextBody
+    await this.querySelector("#element-id")
+
+    this.assert.notOk(await this.turboFormSubmitted)
+  }
+
+  async "test form submission skipped within method=dialog"() {
+    this.listenForDialogButtons()
+    this.listenForFormSubmissions()
+    await this.clickSelector('button[data-open="dialog-method"]')
+    await this.clickSelector('#dialog-method [type="submit"]')
+    await this.nextBeat
+
+    this.assert.notOk(await this.turboFormSubmitted)
+    this.assert.notOk(await this.hasSelector("#dialog-method[open]"))
+  }
+
+  async "test form submission skipped with submitter formmethod=dialog"() {
+    this.listenForDialogButtons()
+    this.listenForFormSubmissions()
+    await this.clickSelector('button[data-open="dialog-formmethod"]')
+    await this.clickSelector('#dialog-formmethod [formmethod="dialog"]')
+    await this.nextBeat
+
+    this.assert.notOk(await this.turboFormSubmitted)
+    this.assert.notOk(await this.hasSelector("#dialog-formmethod[open]"))
+  }
+
+  listenForFormSubmissions() {
+    this.remote.execute(() => addEventListener("turbo:submit-start", function eventListener(event) {
+      removeEventListener("turbo:submit-start", eventListener, false)
+      document.head.insertAdjacentHTML("beforeend", `<meta name="turbo-form-submitted">`)
+    }, false))
+  }
+
+  listenForDialogButtons() {
+    this.remote.execute(() => addEventListener("click", ({ target }) => {
+      if (target instanceof HTMLElement) {
+        const id = target?.getAttribute("data-open")
+        const dialog = document.querySelector("dialog#" + id)
+
+        if (dialog instanceof HTMLDialogElement) dialog.showModal()
+      }
+    }))
+  }
+
+  get turboFormSubmitted(): Promise<boolean> {
+    return this.hasSelector("meta[name=turbo-form-submitted]")
+  }
 }
 
 FormSubmissionTests.registerSuite()
